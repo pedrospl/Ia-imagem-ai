dfebc64d2060c82c29c2a2f15b3701171ba9f749
pedrospl <sousagamerbe@gmail.com>
2025-07-03 23:16:42 -0300

    Create types.ts

---
+++ src/src/types.ts
@@ -0,0 +1,11 @@
+export enum LoadingState {
+  Idle = 'idle',
+  Loading = 'loading',
+  Success = 'success',
+  Error = 'error',
+}
+
+export interface ImageStyle {
+  value: string;
+  label: string;
+}
