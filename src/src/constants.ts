e4f6368ca8bc4e4e1049cd6a1e50b6ac334c9833
pedrospl <sousagamerbe@gmail.com>
2025-07-03 23:21:03 -0300

    Create constants.ts

---
+++ src/src/constants.ts
@@ -0,0 +1,15 @@
+import { ImageStyle } from './types';
+
+export const IMAGE_STYLES: ImageStyle[] = [
+  { value: 'photorealistic', label: 'Photorealistic' },
+  { value: 'digital art', label: 'Digital Art' },
+  { value: 'anime', label: 'Anime' },
+  { value: 'fantasy art', label: 'Fantasy Art' },
+  { value: 'sci-fi', label: 'Sci-Fi' },
+  { value: 'vintage photograph', label: 'Vintage Photo' },
+  { value: 'cyberpunk', label: 'Cyberpunk' },
+  { value: '3d render', label: '3D Render' },
+  { value: 'pixel art', label: 'Pixel Art' },
+  { value: 'watercolor', label: 'Watercolor' },
+  { value: 'low poly', label: 'Low Poly' },
+];
